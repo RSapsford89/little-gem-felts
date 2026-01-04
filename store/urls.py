c3a3321f7e501896e19981458b3688d11f2441c2
RSapsford89 <treborwar@live.co.uk>
2026-01-04 19:48:33 +0000

    updating view for product to filter

--- store/urls.py
+++ store/urls.py
@@ -5,4 +5,5 @@ app_name = 'store'
 
 urlpatterns = [
     path('', views.all_products, name='store'),
+    path('<int:product_id>/', views.product_detail, name='product_detail'),
 ]
--- store/views.py
+++ store/views.py
@@ -9,8 +9,25 @@ def all_products(request):
     :param request: Description
     """
     products = Product.objects.all()
+    main_category = None
+    sub_category = None
+    
 
     context = {
         'products':products,
                }
     return render(request, 'store/products.html', context)
+
+def product_detail(request, product_id):
+    """
+    Docstring for product_detail taken from BoutiqueAdo
+    
+    :param request: Description
+    :param product_id: the product to load detail from
+    """
+    product = get_object_or_404(Product,pk=product_id) # Grab all the Product objects
+    # The context returned to the view...
+    context = {
+        'product': product,
+    }
+    return render(request,'store/product_detail.html',  context)
