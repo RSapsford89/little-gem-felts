e5ae3a71c8eb8091cd0a9f74be4fc423bce01c04
RSapsford89 <treborwar@live.co.uk>
2026-01-04 20:57:05 +0000

    add functioning Product search filter and category buttons

--- store/views.py
+++ store/views.py
@@ -1,22 +1,37 @@
 from django.shortcuts import render, get_object_or_404
+from django.db.models import Q
 from .models import Product
 
 # Create your views here.
 def all_products(request):
     """
     Docstring for all_products
-    Retrieve all products from Product Table.
+    Retrieve all products from Product Table and filter
+    if the filter input has been submitted.
+    Tutorial adapted: https://www.makeuseof.com/add-search-functionality-to-django-apps/ 
     :param request: Description
     """
     products = Product.objects.all()
-    main_category = None
+    filter_query = request.GET.get("filter_input","").strip()
+    category = request.GET.get("category","").strip()
     sub_category = None
     
+    if category.lower() != "all":
+        products = products.filter(main_category__iexact=category)
+
+    if filter_query:
+        products = products.filter(Q(name__icontains=filter_query) | Q(main_category__icontains=filter_query) | Q(description__icontains=filter_query))
 
     context = {
         'products':products,
+        'filter_input':filter_query,
+        'category':category,
                }
     return render(request, 'store/products.html', context)
+
+
+
+
 
 def product_detail(request, product_id):
     """
