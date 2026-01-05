0245cea96249a3ed4558e8772b70fb8652f9c0db
RSapsford89 <treborwar@live.co.uk>
2026-01-05 10:47:13 +0000

    bug: rolled back migrations to implement category table as FK to product.

--- store/models.py
+++ store/models.py
@@ -2,24 +2,38 @@ from django.db import models
 
 
 # Create your models here.
+class Category(models.Model):
+    """
+    Docstring for Category
+    Category names for  use in Product as FK
+    """
+    name = models.CharField(max_length=50, unique=True)
+
+    class  Meta:
+        verbose_name_plural = 'Categories'
+
+    def __str__(self):
+        return self.name
+
+
 class Product(models.Model):
     """
     Docstring for Product
     Model containing product data. Images are contained in Image as FK
     Access images as product.images.all()
+    Category is FK and on_delete is protect found on docs:
+    https://docs.djangoproject.com/en/6.0/ref/models/fields/#django.db.models.ForeignKey.on_delete
     """
     name = models.CharField(max_length=200, blank=False,)
     description = models.TextField(blank=False)
     price = models.DecimalField(max_digits=5, decimal_places=2)
-    main_category = models.CharField(max_length=200, blank=False)
-    sub_category = models.CharField( max_length=200, blank=True, null=True)# Blank & null should be False?
-    # images = models.ImageField( upload_to=None, height_field=None, width_field=None, max_length=None)# This may need to be a FK to another table which pulls in all related files...
+    main_category = models.ForeignKey(Category, on_delete=models.PROTECT, related_name='products')
+    sub_category = models.CharField(max_length=200, blank=True, null=True)
     stock_level = models.IntegerField(default=0, blank=False, null=False)
     delivery_cost = models.DecimalField(max_digits=5, decimal_places=2)
 
-
-def __str__(self):
-    return self.name
+    def __str__(self):
+        return self.name
 
 
 class Images(models.Model):
@@ -33,7 +47,6 @@ class Images(models.Model):
 
     def __str__(self):
         return f"{self.product.name}'s image at position: {self.position}, Primary: {self.primary_image}"
-# class ProductID_ImageID(models.Model):
-#     productID = models.ForeignKey(Product, on_delete=models.CASCADE)
-#     imageID = models.ForeignKey(Images, on_delete=models.CASCADE)
-#     date_added = models.DateTimeField( auto_now_add=True)# add the date it was added
+
+
+
